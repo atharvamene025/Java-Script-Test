bf488f153b909ba0a762bd27bd8bea10f9560579
atharvamene025 <atharvamene@gmail.com>
2024-02-18 14:40:49 +0530

    Updates

---
+++ Test1.js
@@ -0,0 +1,103 @@
+// Q2.
+
+var string="abjba"
+
+    for(i=string[left];i<Math.floor(string[string.right]/2);i++){
+
+        var left=0;
+        var right=string.length-1;
+
+        if(string[left]!=string[right]-1-i){
+          console.log("this is Not a Palindrome")
+        
+        }else{
+            console.log("This is A Palindrome")
+        }
+
+    }
+
+// -----------------------------------------------------------------------------------------------------------------------
+
+// Q3
+
+function calculateSum(){
+var array=[1,2,3,4,5,6]
+
+var sum=0;
+for(i=0;i<=array.length;i++){
+    sum+=i
+    
+    
+}return sum;
+
+}
+
+console.log(calculateSum())
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+    // Q
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Q7 Factorial
+
+function Factorial(){
+
+var Fact=10;
+
+var sum=1;
+for(i=Fact;i>=1;i--){
+
+    sum*=i
+  
+}
+console.log(sum)
+
+}
+
+Factorial()
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Q9 
+
+function calculateFactorial(){
+
+var fact=5;
+var answer=1;
+while(fact>=1){
+
+    answer*=fact;
+    fact--
+}
+console.log(answer)
+}
+
+calculateFactorial()
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Q10 Sorting In Ascending Order
+
+function SortNumbers(){
+
+    var array=[1,6,8,3,2,9,15]
+
+    var a=array[0]
+    var b=array[array.length-1]
+    for(var i=a;i<=b;i++){
+        if(a-b<0){
+
+        }
+    }
+
+}
+
+SortNumbers()
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Q 14
+
+
